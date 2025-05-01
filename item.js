9c70040ffdbcc663446b3fa81920b005858ed1ab
Cindy Nguyen <150876405+cindercindy@users.noreply.github.com>
2025-04-30 23:42:11 -0500

    Update item.js

--- item.js
+++ item.js
@@ -14,9 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById("item-keywords").innerText = item.keywords; // Poem keywords
                 document.getElementById("item-language").innerText = item.language; // Poem language
                 document.getElementById("item-created-date").innerText = item.cdate; // Poem creation date
-                document.getElementById("item-published-date").innerContent = item.pdate || "Not available";
+                document.getElementById("item-published-date").innerText = item.pdate || "Not available";
                 document.getElementById("item-text").innerText = item.text.replace(/\\n/g, '\n'); // Poem text with line breaks
-                document.getElementById("item-url").innerContent = item.url || "Not available";
+                document.getElementById("item-url").innerText = item.url || "Not available";
 
                 // Create JSON-LD metadata
                 const jsonLd = {
